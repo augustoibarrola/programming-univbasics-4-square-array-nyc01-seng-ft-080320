000415030c04a9c386719bf0e7f35ae957a07688
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-17 19:57:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,9 +3,7 @@ def square_array(array)
   counter = 0
   results = Array.new
   while counter <= array.length
-    results.push(Math.sqrt(array[counter].to_i))
+    results.push(Math.sqrt(array[counter]))
     counter += 1
   end
 end
-
-square_array([1, 2, 3])
